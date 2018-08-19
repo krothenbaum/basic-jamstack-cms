00447b907fa9b9c161d194426c66f910fe31d125
krothenbaum <krothenbaum@gmail.com>
2018-08-18 17:37:57 -0700

    Upgrade to v2

---
+++ src/components/layout.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import { StaticQuery, graphql } from 'gatsby'
+import { Container, Column, Row } from 'react-rasta'
+import styled, { ThemeProvider } from 'styled-components'
+
+import Header from './header'
+import './layout.css'
+import theme from '../styles/theme'
+
+const StyledContainer = styled(Container)`
+  background-color: ${theme.colors.blueGreen};
+`
+
+const Layout = ({ children }) => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query LayoutQuery {
+          site {
+            siteMetadata {
+              title
+            }
+          }
+        }
+      `}
+      render={data => (
+        <ThemeProvider theme={theme}>
+          <StyledContainer fluid={true}>
+            <Helmet
+              title={data.site.siteMetadata.title}
+              meta={[
+                { name: 'description', content: 'Sample' },
+                { name: 'keywords', content: 'sample, something' },
+              ]}
+            />
+            <Row>
+              <Column>
+                <Header siteTitle={data.site.siteMetadata.title} />
+              </Column>
+            </Row>
+            {children}
+          </StyledContainer>
+        </ThemeProvider>
+      )}
+    />
+  )
+}
+
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,29 @@
 import React from 'react'
-import Link from 'gatsby-link'
-import { Column, Row } from 'react-rasta'
+import { graphql } from 'gatsby'
+import { Row } from 'react-rasta'
+import Layout from '../components/Layout'
 import Card from '../components/Card'
 
 const IndexPage = ({ data }) => {
   const { edges } = data.allMarkdownRemark
   return (
-    <Row justifyContent="center" direction="column">
-      {edges.map((item, index) => {
-        console.log(item.node)
-        return (
-          <Card
-            title={item.node.frontmatter.title}
-            imgSrc={item.node.frontmatter.imgSrc}
-            html={item.node.html}
-            key={index}
-          />
-        )
-      })}
-      {/* <Card title="Hi people" body="Welcome to your new Gatsby site. Now go build something great." imgSrc="https://source.unsplash.com/random/200x200"/>
+    <Layout>
+      <Row justifyContent="center" direction="column">
+        {edges.map((item, index) => {
+          console.log(item.node)
+          return (
+            <Card
+              title={item.node.frontmatter.title}
+              imgSrc={item.node.frontmatter.imgSrc}
+              html={item.node.html}
+              key={index}
+            />
+          )
+        })}
+        {/* <Card title="Hi people" body="Welcome to your new Gatsby site. Now go build something great." imgSrc="https://source.unsplash.com/random/200x200"/>
     <Card title="Sup FuckFace" body="Still Here? Great, keep working." imgSrc="https://source.unsplash.com/random/200x200"/> */}
-    </Row>
+      </Row>
+    </Layout>
   )
 }
 
