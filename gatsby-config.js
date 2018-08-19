64778f562e0d839f791b9124fc191b5a826b552b
krothenbaum <krothenbaum@gmail.com>
2018-08-18 20:48:01 -0700

    build fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,14 +9,6 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     `gatsby-plugin-styled-components`,
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     path: `${__dirname}/src/markdown`,
-    //     name: 'markdown-pages',
-    //   },
-    // },
-    // `gatsby-transformer-remark`,
     {
       resolve: `gatsby-source-graphcms`,
       options: {
